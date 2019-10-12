82ffb512ea6a724bdfb6fd79a2160fd4018cdd2e
Jonathan Dana <jonathandana27@gmail.com>
2019-10-12 21:02:21 +0300

    add state hoks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState,useEffect} from 'react';
 import '../assets/styles/App.scss';
 import Header from "../components/Header";
 import Search from "../components/Search";
@@ -9,37 +9,50 @@ import Carousel from "../components/Carousel";
 import CarouselItem from "../components/CarouselItem";
 import Footer from "../components/Footer";
 
-const App = () => (
-    <div className="App">
-        <Header/>
-        <Search/>
-        <Categories title="Mi lista">
-            <Carousel>
-                <CarouselItem/>
-                <CarouselItem/>
-                <CarouselItem/>
-                <CarouselItem/>
-                <CarouselItem/>
-            </Carousel>
-        </Categories>
+const App = () => {
+    const initialState = {
+        "mylist": [],
+        "trends": []
+    };
 
-        <Categories title="Tendencias">
-            <Carousel>
-                <CarouselItem/>
-                <CarouselItem/>
-                <CarouselItem/>
-            </Carousel>
-        </Categories>
+    const [videos,setVideos] = useState(initialState);
 
-        <Categories title="Originales de Platzi video">
-            <Carousel>
-                <CarouselItem/>
-                <CarouselItem/>
-            </Carousel>
-        </Categories>
-        <Footer/>
-    </div>
-);
+    useEffect(()=>{
+        fetch('http://localhost:3000/initalState')
+            .then(res => res.json())
+            .then(data => setVideos(data))
+    },[]);
 
+
+    return (
+        <div className="App">
+            <Header/>
+            <Search/>
+            {videos.mylist.length > 0 &&
+                <Categories title="Mi lista">
+                    <Carousel>
+
+                    </Carousel>
+                </Categories>
+            }
+            <Categories title="Tendencias">
+                <Carousel>
+                    {
+                        videos.trends.map(video =>
+                                <CarouselItem key={video.id} {...video}/>
+                            )
+                    }
+                </Carousel>
+            </Categories>
+
+            <Categories title="Originales de Platzi video">
+                <Carousel>
+
+                </Carousel>
+            </Categories>
+            <Footer/>
+        </div>
+    );
+}
 
 export default App;
