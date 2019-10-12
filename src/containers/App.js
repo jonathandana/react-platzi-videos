154cb9c32501dab491b95d3155e4476958edad8b
Jonathan Dana <jonathandana27@gmail.com>
2019-10-12 15:07:10 +0300

    components carousels

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,10 +2,23 @@ import React from 'react';
 import '../assets/styles/App.scss';
 import Header from "../components/Header";
 import Search from "../components/Search";
+
+//Carousel.
+import Categories from "../components/Categories";
+import Carousel from "../components/Carousel";
+import CarouselItem from "../components/CarouselItem";
+
 const App = () => (
     <div className="App">
         <Header/>
         <Search/>
+        <Categories>
+            <Carousel>
+                <CarouselItem/>
+                <CarouselItem/>
+                <CarouselItem/>
+            </Carousel>
+        </Categories>
     </div>
 );
 
