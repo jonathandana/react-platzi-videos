b39504575d0d6f98ba12b70493d120815c0a7aed
Jonathan Dana <jonathan@beeriprint.co.il>
2019-10-22 15:04:46 +0300

    use component Link

--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import googleIcon from '../assets/static/google-icon.png';
 import twitterIcon from '../assets/static/twitter-icon.png';
 import '../assets/styles/components/Login.scss';
+import { Link } from 'react-router-dom';
 
 const Login = ()=> (
     <section className="login">
@@ -31,7 +32,7 @@ const Login = ()=> (
             </section>
             <p className="login__container--register">
                 No tienes ninguna cuenta
-                <a href="">Regístrate</a>
+                <Link to="/register">Regístrate</Link>
             </p>
         </section>
     </section>
