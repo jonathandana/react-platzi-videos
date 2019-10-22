5394d40ad5a42dd69e3439ececf6e1f3d62272a9
Jonathan Dana <jonathan@beeriprint.co.il>
2019-10-22 14:07:36 +0300

    add component 404

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from "../containers/Home";
 import Login from "../containers/Login";
 import Register from "../containers/Register";
+import notFound from '../containers/notFound';
 
 const App = ()=>(
     <BrowserRouter>
+      <Switch>
         <Route exact path="/" component={Home}/>
         <Route exact path="/login" component={Login}/>
         <Route exact path="/register" component={Register}/>
+        <Route component={notFound}/>
+      </Switch>
     </BrowserRouter>
 );
 
